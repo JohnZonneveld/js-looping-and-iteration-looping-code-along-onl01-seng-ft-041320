2878225dad5215132fd3aef6d11a456d296d11b6
raspimeteo <jjczon@gmail.com>
2020-07-13 19:00:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards
+}
+
+function countDown(number) {
+    for (let i = number; i > -1; i--) {
+        console.log(i)
+    }
+}
+ 
+
